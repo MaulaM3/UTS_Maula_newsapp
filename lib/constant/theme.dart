182281a73a3827cbf2a203adb439c9eb5f31e557
MaulaM3 <144292473+MaulaM3@users.noreply.github.com>
2024-11-05 09:15:02 +0700

    first init github

---
+++ lib/constant/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Warna dasar aplikasi
+Color whiteColor = const Color(0xFFFFFFFF);
+Color blackColor = const Color(0xFF000000);
+Color greyColor = const Color(0xFF82868E);
+Color lightGreyColor = const Color(0xFF969696);
+Color primaryColor = const Color(0xFFF39E3A);
+
+// Text Styles
+TextStyle whiteTextStyle = GoogleFonts.lato(
+  color: whiteColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.montserrat(
+  color: blackColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.montserrat(
+  color: greyColor,
+);
+
+TextStyle lightGreyTextStyle = GoogleFonts.montserrat(
+  color: lightGreyColor,
+);
---
+++ lib/pages/news_detail_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class NewsDetailPage extends StatefulWidget {
+  final String link;
+  const NewsDetailPage({
+    super.key,
+    required this.link,
+  });
+
+  @override
+  State<NewsDetailPage> createState() => _NewsDetailPageState();
+}
+
+class _NewsDetailPageState extends State<NewsDetailPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    // Inisialisasi WebViewController dengan cara yang kompatibel dengan versi terbaru
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.link));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News Detail'),
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
